84ad2a846e366dea3787467309c0622a9fab4d8e
nitron99 <nitin99bop2cod2f@gmail.com>
2021-06-27 23:52:21 +0530

    minor chnges

--- src/components/pages/homepage/homepage.js
+++ src/components/pages/homepage/homepage.js
@@ -6,7 +6,7 @@ class home extends Component{
         return(
             <div className="home">
                 <div className="home_img">
-                    <img src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"/>
+                    <img src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt=""/>
                 </div>
                 <div className="divider"></div>
                     Content Available on
